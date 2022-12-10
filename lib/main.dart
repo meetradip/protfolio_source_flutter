a8caa7c5171bfb83c0555633c3313abc9a18026a
Riduan_Radip <kingkillerriduan@gmail>
2022-12-10 23:19:01 +0600

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class _MyAppState extends State<MyApp> {
             ],
           )),
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Riduan Rahman Radip',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
